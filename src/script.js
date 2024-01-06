7f04608dcd9eee8d5e39c409d3a45e36135a6bb2
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-01-06 03:50:16 -0800

    testing

--- src/script.js
+++ src/script.js
@@ -8,15 +8,20 @@ document.getElementById("create").addEventListener("click", () => {
 });
 
 document.getElementById("join").addEventListener("click", () => {
+    console.log("connecting");
     peer.connect(document.getElementById("inp").value);
-    
+    console.log("sent connection");
 });
 
 peer.on("connection", (conn) => {
+    console.log("received connection");
     conn.on("open", () => {
+        console.log("sending data");
         conn.send("from " + peer.id);
+        console.log("sent data");
 
         conn.on("data", (data) => {
+            console.log("received data");
             console.log(data);
         });
     });
