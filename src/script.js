438697d4979de340baf2eb4112800dd906763c8a
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-01-18 19:03:46 -0800

    LETS GOO IT WORKS both connections need to be initialized first, think about it like 'Create profile' > 'Connect to your friend' > 'Play the game'

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
-document.getElementById("create").addEventListener("click", () => {
-    const peer = new Peer();
+let currentConnection = null;
+let peer = null;
+
+function initializePeer() {
+    peer = new Peer();
 
     peer.on("open", (id) => {
         console.log(`My peer ID is: ${id}`);
@@ -10,16 +13,21 @@ document.getElementById("create").addEventListener("click", () => {
         console.log("Connection received");
         setupConnectionHandler(connection);
     });
-});
+}
+
+document.getElementById("create").addEventListener("click", initializePeer);
 
 document.getElementById("join").addEventListener("click", () => {
+    if (!peer) {
+        initializePeer();
+    }
+
     const otherid = document.getElementById("inp").value;
     if (!otherid) {
-        alert("Please enter an ID to conect to");
+        alert("Please enter an ID to connect to");
         return;
     }
 
-    const peer = new Peer();
     const connection = peer.connect(otherid);
     console.log(`Sent connection to ${otherid}`);
 
@@ -29,13 +37,23 @@ document.getElementById("join").addEventListener("click", () => {
 function setupConnectionHandler(conn) {
     currentConnection = conn;
 
-    conn.on('open', () => {
-        console.log('Connection established.');
+    conn.on("open", () => {
+        console.log("Connection established.");
     });
 
     // Receive messages
-    conn.on('data', (data) => {
-        console.log('Received:', data);
+    conn.on("data", (data) => {
+        console.log("Received:", data);
+        alert(data);
     });
 }
+
+document.getElementById("send").addEventListener("click", () => {
+    if (currentConnection?.open) {
+        const data = prompt("Enter message to send", "Hello World");
+        currentConnection.send(data);
+    } else {
+        console.log("No active connection.");
+    }
+});
 
